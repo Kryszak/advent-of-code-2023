1e1fbdd5fbb6547179563bf615134f6ad7e41cd4
Kryszak <prajskrzysztof@gmail.com>
2023-12-15 13:53:26 +0100

    day 15 solution

--- day_15/part1.go
+++ day_15/part1.go
@@ -1,7 +1,43 @@
 package day15
 
+import (
+	"strings"
+
+	"github.com/Kryszak/aoc2023/common"
+)
+
+func loadInput(path string) [][]rune {
+	var input [][]rune
+	fileScanner := common.FileScanner(path)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		for _, value := range strings.Split(line, ",") {
+			input = append(input, []rune(value))
+		}
+	}
+
+	return input
+}
+
+func calculateStepHash(step []rune) int {
+	result := 0
+	for _, character := range step {
+		result += int(character)
+		result *= 17
+		result %= 256
+	}
+	return result
+}
+
 func Part1(path string) int {
 	answer := 0
+
+	input := loadInput(path)
+
+	for _, input := range input {
+		answer += calculateStepHash(input)
+	}
 
 	return answer
 }
--- day_15/part2.go
+++ day_15/part2.go
@@ -1,7 +1,109 @@
 package day15
 
+import (
+	"slices"
+	"strconv"
+)
+
+type step struct {
+	label       []rune
+	operation   rune
+	focalLength int
+}
+
+type label struct {
+	label string
+	power int
+}
+
+func (a label) equal(b string) bool {
+	return a.label == b
+}
+
+func parseDigit(character rune) int {
+	value, _ := strconv.Atoi(string(character))
+	return value
+}
+
+func parseSteps(input [][]rune) []step {
+	var steps []step
+
+	for _, raw := range input {
+		var label []rune
+		var operation rune
+		var focalLength int
+		if slices.Contains(raw, '=') {
+			i := 0
+			for raw[i] != '=' {
+				label = append(label, raw[i])
+				i++
+			}
+			operation = '='
+			focalLength = parseDigit(raw[len(raw)-1])
+		}
+		if slices.Contains(raw, '-') {
+			label = raw[0 : len(raw)-1]
+			operation = '-'
+		}
+		steps = append(steps, step{label, operation, focalLength})
+	}
+
+	return steps
+}
+
+func installLenses(steps []step) map[int][]label {
+	boxes := make(map[int][]label, 256)
+
+	for _, step := range steps {
+		boxNumber := calculateStepHash(step.label)
+		lenLabel := string(step.label)
+
+		if step.operation == '=' {
+			lensIndex := slices.IndexFunc(boxes[boxNumber], func(l label) bool {
+				return l.equal(lenLabel)
+			})
+			if lensIndex != -1 {
+				boxes[boxNumber][lensIndex].power = step.focalLength
+			} else {
+				boxes[boxNumber] = append(boxes[boxNumber], label{lenLabel, step.focalLength})
+			}
+		} else {
+			boxes[boxNumber] = slices.DeleteFunc(boxes[boxNumber], func(l label) bool {
+				return l.equal(lenLabel)
+			})
+		}
+	}
+
+	for i := range boxes {
+		if len(boxes[i]) == 0 {
+			delete(boxes, i)
+		}
+	}
+
+	return boxes
+}
+
+func calculateBoxFocusingPower(boxIndex int, lenses []label) int {
+	result := 0
+
+	for lensIndex, lens := range lenses {
+		result += (boxIndex + 1) * (lensIndex + 1) * lens.power
+	}
+
+	return result
+}
+
 func Part2(path string) int {
 	answer := 0
+
+	input := loadInput(path)
+	steps := parseSteps(input)
+
+	boxes := installLenses(steps)
+
+	for i, box := range boxes {
+		answer += calculateBoxFocusingPower(i, box)
+	}
 
 	return answer
 }
