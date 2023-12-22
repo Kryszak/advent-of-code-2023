fcc9ab62df63d3b7c05188d77490ef1a2ac5c4fe
Kryszak <prajskrzysztof@gmail.com>
2023-12-22 11:39:58 +0100

    day 21 part1

--- day_21/day_21_test.go
+++ day_21/day_21_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestDay21(t *testing.T) {
 	t.Run("Part1", func(t *testing.T) {
 		got := Part1("input.txt")
-		want := 0
+		want := 16 
 
 		if got != want {
 			t.Errorf("got %v wanted %v", got, want)
--- day_21/part1.go
+++ day_21/part1.go
@@ -1,6 +1,78 @@
 package day21
 
+import (
+	"fmt"
+
+	"github.com/Kryszak/aoc2023/common"
+)
+
+type point struct {
+	x, y int
+}
+
+func loadGarden(path string) [][]rune {
+	var garden [][]rune
+	scanner := common.FileScanner(path)
+
+	for scanner.Scan() {
+		row := []rune(scanner.Text())
+		garden = append(garden, row)
+	}
+
+	return garden
+}
+
+func printGarden(garden [][]rune) {
+	for _, row := range garden {
+		for _, col := range row {
+			fmt.Print(string(col))
+		}
+		fmt.Println()
+	}
+}
+
+func getStartingPoint(garden [][]rune) (x, y int) {
+	for x, row := range garden {
+		for y, col := range row {
+			if col == 'S' {
+				return x, y
+			}
+		}
+	}
+	panic("Starting point not found in garden!")
+}
+
+func isInside(garden [][]rune, p point) bool {
+	return p.x >= 0 && p.x < len(garden) && p.y >= 0 && p.y < len(garden[p.x])
+}
+
+func step(garden [][]rune, visited map[point]bool) map[point]bool {
+	visitedPoints := make(map[point]bool)
+
+	for p := range visited {
+		for _, candidate := range [4]point{{p.x - 1, p.y}, {p.x + 1, p.y}, {p.x, p.y - 1}, {p.x, p.y + 1}} {
+			if isInside(garden, candidate) && garden[candidate.x][candidate.y] != '#' {
+				visitedPoints[candidate] = true
+			}
+		}
+	}
+
+	return visitedPoints
+}
+
 func Part1(path string) (answer int) {
+	garden := loadGarden(path)
+	x, y := getStartingPoint(garden)
+
+	visited := make(map[point]bool)
+	visited[point{x, y}] = true
+
+	// change for 64 for real-data solution
+	for i := 0; i < 6; i++ {
+		visited = step(garden, visited)
+	}
+
+	answer = len(visited)
 
 	return answer
 }
